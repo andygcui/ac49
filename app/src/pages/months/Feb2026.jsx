26c6c9237d2219fb709197e59219fa7dc8672019
andygcui <andygcui@gmail.com>
2026-03-08 16:50:59 -0400

    update feb layout

--- app/src/pages/months/Feb2026.jsx
+++ app/src/pages/months/Feb2026.jsx
@@ -22,37 +22,48 @@ function Feb2026() {
               <p className="text-gray-600 mb-2 italic">
                 bicker, benz, boooze, jobs, buddys, blizzard
               </p>
-              <p className="text-gray-600 mb-2">
-                <br />
-                princeton, nj <br />
-                new york, ny <br />
-              </p>
             </header>
 
-            <div className="flex justify-center gap-10">
-              <p className="text-gray-600">
-              super bowl lx <br />
-              winter olympics<br />
-              </p>
-              <p className="text-gray-600">
-                dreams, fleetwood mac <br />
-                piss in the wind, joji</p>
-               
+            {/* Artsy minimal layout */}
+            <div className="max-w-2xl mx-auto space-y-12">
+              
+              {/* Locations - left aligned */}
+              <div className="pl-14">
+                <p className="text-gray-600 leading-relaxed">
+                super bowl lx<br />
+                winter olympics
+                </p>
+              </div>
 
-                <br />
-      
+              {/* Shows - centered */}
+              <div className="text-center">
+                <p className="text-gray-600">
+                princeton, nj<br />
+                new york, ny
+                </p>
+              </div>
 
-              <iframe 
-            width="80" 
-            height="80" 
-            src="https://www.youtube.com/embed/1qmrq6I_jHI?start=53&autoplay=1" 
-            title="raindance" 
-            frameBorder="0" 
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-            allowFullScreen>
-          </iframe>
-          
-              <br /><br /><br /> <br /><br />
+              {/* Music - right aligned */}
+              <div className="pr-14 text-right">
+                <p className="text-gray-600 leading-relaxed">
+                  dreams, fleetwood mac<br />
+                  piss in the wind, joji
+                </p>
+              </div>
+
+              {/* YouTube embed - centered, smaller */}
+              <div className="flex justify-center pt-4">
+                <iframe 
+                  width="80" 
+                  height="80" 
+                  src="https://www.youtube.com/embed/1qmrq6I_jHI?start=53&autoplay=1" 
+                  title="raindance" 
+                  frameBorder="0" 
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                  allowFullScreen>
+                </iframe>
+              </div>
+
             </div>
           </div>
 
