396d2fb54ecf51c8b631e0f4bcbf23f8dd045443
andygcui <andygcui@gmail.com>
2026-03-21 11:45:45 -0400

    mar update

--- app/src/pages/months/Mar2026.jsx
+++ app/src/pages/months/Mar2026.jsx
@@ -5,6 +5,21 @@ function Mar2026() {
         <div className="container mx-auto px-4 py-16">
           <div className="max-w-4xl mx-auto">
 
+          <div className="flex justify-center"> 
+              <video 
+              src="/uploads/mar26.MOV"
+              autoPlay
+              loop
+              muted
+              playsInline
+              alt="mar"
+              controls={false}
+              className="max-w-xs scale-40"
+              style={{width: '20%', height: 'auto'}}>
+              </video>
+            </div>
+
+
             <header className="text-center mb-16">
 
               <h1 className="text-gray-900 mb-4">
@@ -12,13 +27,14 @@ function Mar2026() {
                 march 2026
               </h1>
               <p className="text-gray-600 mb-2 italic">
-                briger, borgs, midterms, 
+                briger, borgs, job, talks
               </p>
               <p className="text-gray-600 mb-2">
                 <br />
                 princeton, nj <br />
                 philadelphia, pa <br />
-                potomac, md <br />
+                silver spring, md <br />
+                georgetown, dc <br />
                 ithaca, ny <br />
               </p>
             </header>
@@ -26,10 +42,16 @@ function Mar2026() {
             <div className="flex justify-center gap-10">
               <p className="text-gray-600">
               indian wells <br />
+              march madness <br />
+              suits
               </p>
+
               <p className="text-gray-600">
-                who's the fool, malcom todd</p>
-               
+                who's the fool, malcom todd <br />
+                sick love, red hot chili peppers <br />
+                keep looking, sade
+                </p>
+
                 <br />
       
               <iframe 
